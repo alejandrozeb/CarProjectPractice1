4018445db327196e0f43c7df2d7f6d778845ad92
alejandro zeballos aguilar <alejandrojzeballos@gmail.com>
2022-05-05 19:27:33 -0400

    {add} adding atributes to CarClass

--- CarProjectsPractice1/Car.cs
+++ CarProjectsPractice1/Car.cs
@@ -6,6 +6,18 @@ namespace CarProjectsPractice1
 {
     class Car
     {
-       
+        private int registrationNumber;
+        private string model;
+        private DateTime year;
+        private int maxSpeed;
+        private int minSpeed;
+        private int fuelLevel;
+        private int waterLevel;
+        private int oilLevel;
+        private string color;
+        private string type;
+        private int numberDoors;
+
+
     }
 }
